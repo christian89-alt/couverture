5a985b19da6d2d95483dbc318d6ea54d134229b5
christian89-alt <c.duboc43@gmail.com>
2026-03-18 11:14:39 +0100

    changement branche

--- partial/header/header.php
+++ partial/header/header.php
@@ -1,11 +1,9 @@
-
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Index - Christian Service </title>
+  <title>Couverture, Bardage , Etancheité - Christian Service </title>
   <meta name="description" content="">
   <meta name="keywords" content="">
 
@@ -31,3 +29,96 @@
 
 
 </head>
+
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">Christian DUBOC Services </h1> <span>.</span>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.html" class="active">Accueil</a></li>
+          <li><a href="moncv.html">Mon CV </a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+      <div class="info d-flex align-items-center">
+        <div class="container">
+          <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-lg-6 text-center">
+              <h2>Christian Services </h2>
+              <p>Que vous soyez professionnel ou particulier je vous propose mes services. J'interviens dans le nettoyage de vos toits, l'entretien de votre résidence, ou le renforcement de vos équipes.</p>
+              <a href="#get-started" class="btn-get-started">Contact</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
+        </div>
+
+        <div class="carousel-item active">
+          <img src="assets/img/hero-carousel/hero-carousel-2.jpg" alt="">
+        </div>
+
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-3.jpg" alt="">
+        </div>
+
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-4.jpg" alt="">
+        </div>
+
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-5.jpg" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-6.jpg" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-7.jpg" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-8.jpg" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-9.jpg" alt="">
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/hero-carousel/hero-carousel-10.jpg" alt="">
+        </div>
+
+        <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+        </a>
+
+      </div>
+
+    </section><!-- /Hero Section -->
+
+    ?>
