9078f1c8e53af1a84592b1fd2a3671a1cfcd1ca9
christian89-alt <c.duboc43@gmail.com>
2026-05-15 17:20:34 +0200

    update

--- footer.php
+++ footer.php
@@ -1,92 +1,97 @@
     <footer id="footer" class="footer dark-background">
 
-        <div class="container footer-top">
-          <div class="row gy-4">
-            <div class="col-lg-4 col-md-6 footer-about">
-              <a href="index.html" class="logo d-flex align-items-center">
-                <span class="sitename">C & D Services</span>
-              </a>
-              <div class="footer-contact pt-3">
-                <p>7 A rue Folle Peine </p>
-                <p>51 100 Reims</p>
-                <p class="mt-3"><strong>Téléphone:</strong> <span>+33 7 67 53 36 25</span></p>
-                <p><strong>Email:</strong> <span>c.duboc43@gmail.com</span></p>
-              </div>
-              <div class="social-links d-flex mt-4">
-                <a href=""><i class="bi bi-twitter-x"></i></a>
-                <a href=""><i class="bi bi-facebook"></i></a>
-                <a href=""><i class="bi bi-instagram"></i></a>
-                <a href=""><i class="bi bi-linkedin"></i></a>
-              </div>
+      <div class="container footer-top">
+        <div class="row gy-4">
+          <div class="col-lg-4 col-md-6 footer-about">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <span class="sitename">C & D Services</span>
+            </a>
+            <div class="footer-contact pt-3">
+              <p>7 A rue Folle Peine </p>
+              <p>51 100 Reims</p>
+              <p class="mt-3"><strong>Téléphone:</strong> <span>+33 7 67 53 36 25</span></p>
+              <p><strong>Email:</strong> <span>c.duboc43@gmail.com</span></p>
             </div>
-
-            <div class="col-lg-2 col-md-3 footer-links">
-              <h4>Liens utiles</h4>
-              <ul>
-                <li><a href="index.php" class="active">Accueil</a></li>
-                <li><a href="moncv.php">Mon CV </a></li>
-                <li><a href="posepanneaux.php">Energie solaire</a></li>
-                <li><a href="etancheite.php">Etancheité</a></li>
-                <li><a href="auditenergetique.php">Rénovation Electrique</a></li>
-                <li><a href="contact.php">Contact</a></li>
-
-              </ul>
+            <div class="social-links d-flex mt-4">
+              <a href=""><i class="bi bi-twitter-x"></i></a>
+              <a href=""><i class="bi bi-facebook"></i></a>
+              <a href=""><i class="bi bi-instagram"></i></a>
+              <a href=""><i class="bi bi-linkedin"></i></a>
             </div>
+          </div>
 
-            <div class="col-lg-2 col-md-3 footer-links">
-              <h4>Mes services</h4>
-              <ul>
-                <li><a href="servicepro.php">Pour les professionnels</a></li>
-                
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Liens utiles</h4>
+            <ul>
+              <li><a href="index.php" class="active">Accueil</a></li>
+              <li><a href="moncv.php">Mon CV </a></li>
+              <li><a href="posepanneaux.php">Energie solaire</a></li>
+              <li><a href="etancheite.php">Etancheité</a></li>
+              <li><a href="auditenergetique.php">Rénovation Electrique</a></li>
+              <li><a href="contact.php">Contact</a></li>
 
-              </ul>
-            </div>
+            </ul>
+          </div>
 
-            <div class="col-lg-2 col-md-3 footer-links">
-              <h4>Quelques liens utiles</h4>
-              <ul>
-                <li><a href="#">MaPrime Rénove</a></li>
-                <li><a href="#">Anah</a></li>
-                <li><a href="termetcondition.php">Termes et conditions</a></li>
-                <li><a href="policyprivacy.phpl">Respect de la vie privée </a></li>
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Mes services</h4>
+            <ul>
+              <li><a href="servicepro.php">Pour les professionnels</a></li>
 
-              </ul>
-            </div>
 
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Quelques liens utiles</h4>
+            <ul>
+              <li><a href="#">MaPrime Rénove</a></li>
+              <li><a href="#">Anah</a></li>
+              <li><a href="termetcondition.php">Termes et conditions</a></li>
+              <li><a href="policyprivacy.phpl">Respect de la vie privée </a></li>
+
+            </ul>
           </div>
 
         </div>
-        </div>
 
-        <div class="container copyright text-center mt-4">
-          <p>© <span>Tous droits réservés à </span> <strong class="px-1 sitename">Christian Duboc Services</strong>
-            <span>All Rights Reserved</span>
-          </p>
+      </div>
+      </div>
 
-        </div>
+      <div class="container copyright text-center mt-4">
+        <p>© <span>Tous droits réservés à </span> <strong class="px-1 sitename">Christian Duboc Services</strong>
+          <span>All Rights Reserved</span>
+        </p>
 
-      </footer>
+      </div>
 
-      <!-- Scroll Top -->
-      <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-          class="bi bi-arrow-up-short"></i></a>
+    </footer>
 
-      <!-- Preloader -->
-      <div id="preloader"></div>
+    <!-- Scroll Top -->
+    <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
-      <!-- Vendor JS Files -->
-      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="assets/vendor/php-email-form/validate.js"></script>
-      <script src="assets/vendor/aos/aos.js"></script>
-      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-      <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <!-- Preloader -->
+    <div id="preloader"></div>
 
-      <!-- Main JS File -->
-      <script src="assets/js/main.js"></script>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
 
-  </body>
+    <!-- Main JS File -->
+    <script src="assets/js/main.js"></script>
 
-</html>
+    <!--RJPD -->
+    <script id="Cookiebot" data-culture="EN" src="https://consent.cookiebot.com/uc.js"
+      data-cbid="00000000-0000-0000-0000-000000000000" type="text/javascript"></script>
+
+
+    </body>
+
+    </html>
