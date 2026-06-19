46b734017d892ea7b7b10128ad18d6e44cdd5e66
christian89-alt <c.duboc43@gmail.com>
2026-06-19 18:30:34 +0200

    update

---
+++ php/public/partial/caroussel.php
@@ -0,0 +1,63 @@
+      <section id="hero" class="hero section dark-background">
+
+        <div class="info d-flex align-items-center">
+          <div class="container">
+            <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="100">
+              <div class="col-lg-6 text-center">
+                <h2>Christian Duboc Services </h2>
+               <p>Je vous propose mes services pour vous accompagner dans la gestion, ou la réalisation de vos chantiers. J'interviens auprès de vos entreprises et viens consolider vos ressources humaines ou sous traiter vos chantiers. 
+                  Qu'il s'agisse de locaux industriels ou résidentiels, je vous propose un ensemble de solutions dont le but est d'optimiser votre gestion quotidienne.</p>
+                <a href="contact.html" class="btn-get-started">Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
+          </div>
+
+          <div class="carousel-item active">
+            <img src="assets/img/hero-carousel/hero-carousel-2.jpg" alt="">
+          </div>
+
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-3.jpg" alt="">
+          </div>
+
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-4.jpg" alt="">
+          </div>
+
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-5.jpg" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-6.jpg" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-7.jpg" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-8.jpg" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-9.jpg" alt="">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/hero-carousel-10.jpg" alt="">
+          </div>
+
+          <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+          </a>
+
+          <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+          </a>
+
+        </div>
+
+      </section><!-- /Hero Section -->
---
+++ php/public/partial/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Couverture, Bardage , Etancheité - Christian Service </title>
+  <meta name="description" content="entretien de vos toitures sur l'ensemble du territoire rémois et français">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon_1.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+  <!-- RJPD -->
+
+  <link href="https://consent.cookiebot.com/uc.js?cbid=00000000-0000-0000-0000-000000000000
+">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!--RJPD -->
+  <script id="Cookiebot" data-culture="EN" src="https://consent.cookiebot.com/uc.js"
+    data-cbid="00000000-0000-0000-0000-000000000000" type="text/javascript"></script>
+
+
+</head>
+
+
+<body class="index-page">
+
+  <body class="services-page">
+
+    <header id="header" class="header d-flex align-items-center fixed-top">
+      <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+        <a href="index.html" class="logo d-flex align-items-center">
+
+          <h1 class="sitename">Christian Duboc Services </h1> <span>.</span>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+          <ul>
+            <li><a href="index.html" class="active">Accueil</a></li>
+            <li><a href="moncv.html">Mon CV </a></li>
+            <li><a href="posepanneaux.html">Solaire</a></li>
+            <li><a href="gestionoffshore.html">Conseil</a></li>
+            <li><a href="auditenergetique.html">Electricite</a></li>
+            <li><a href="couverture.html">Couverture</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+      </div>
+    </header>
