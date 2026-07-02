edf9a785549540252dd2e695bfdc70dac1818fd3
christian89-alt <c.duboc43@gmail.com>
2026-07-02 15:40:50 +0200

    update

--- php/public/Layout/caroussel.php
+++ php/public/Layout/caroussel.php
@@ -5,8 +5,10 @@
             <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="100">
               <div class="col-lg-6 text-center">
                 <h2>Christian Duboc Services </h2>
-               <p>Je vous propose mes services pour vous accompagner dans la gestion, ou la réalisation de vos chantiers. J'interviens auprès de vos entreprises et viens consolider vos ressources humaines ou sous traiter vos chantiers. 
-                  Qu'il s'agisse de locaux industriels ou résidentiels, je vous propose un ensemble de solutions dont le but est d'optimiser votre gestion quotidienne.</p>
+                <p>Spécialiste du batiment je vous accompagnes dans la gestion, ou la réalisation de vos
+                  chantiers en m'intégrant à vos équipes en tant que renfort. Je vous aide aussi dans la création de vos
+                  holdings en Espagne et dans la réalisation d'investissements immobiliers locatif ou non.
+                  traiter vos chantiers. </p>
                 <a href="contact.html" class="btn-get-started">Contact</a>
               </div>
             </div>
@@ -16,40 +18,39 @@
         <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
 
           <div class="carousel-item">
-            <img src="assets/img/hero-carousel/accompahnement entreprise.jpg" alt="">
+            <img src="assets/img/hero-carousel/accompahnement entreprise.jpg"
+              alt="conseil entreprise, conseil en investissement, gestion et développement d'entreprise">
           </div>
 
           <div class="carousel-item active">
-            <img src="assets/img/hero-carousel/acquisition immobiliere.jpg" alt="">
+            <img src="assets/img/hero-carousel/acquisition immobiliere.jpg"
+              alt="conseil en acquisition immobilièer en espagne,immobilier en vente en catalogne, conseil en acquisition imobilière en catalogne">
           </div>
 
           <div class="carousel-item">
-            <img src="assets/img/hero-carousel/conduite de chantier.jpg" alt="">
+            <img src="assets/img/hero-carousel/conduite de chantier.jpg"
+              alt="gestion de chantier, conseil ene gestion de chantier">
           </div>
 
           <div class="carousel-item">
-            <img src="assets/img/hero-carousel/pose de panneaux solaire.jpg" alt="">
+            <img src="assets/img/hero-carousel/pose de panneaux solaire.jpg"
+              alt="pose de panneaux solaire, conseil en pose de panneaux solaires">
           </div>
 
           <div class="carousel-item">
-            <img src="assets/img/hero-carousel/entretiens de piscine.jpg" alt="">
+            <img src="assets/img/hero-carousel/entretiens de piscine.jpg"
+              alt="entretien et création de piscine, entretien de piscine">
           </div>
           <div class="carousel-item">
-            <img src="assets/img/hero-carousel/entretiens toiture.jpg" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="assets/img/hero-carousel/page-title-bg.jpg" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="assets/img/hero-carousel/plaquiste.jpg" alt="">
-          </div>
-          <div class="carousel-item">
-            <img src="assets/img/hero-carousel/maintenance electrique.jpg" alt="">
+            <img src="assets/img/hero-carousel/entretiens toiture.jpg" alt="entretien de toiture, recherche de fuite, ">
           </div>
 
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/plaquiste.jpg" alt="pose de placo, isolation intérieur, gestion de l'isolation intérieur">
+          </div>
+          <div class="carousel-item">
+            <img src="assets/img/hero-carousel/maintenance electrique.jpg" alt="rénovation électrique, gestion de l'energie,pose de borne, recherche de panne électrique">
+          </div>
           <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
             <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
           </a>
